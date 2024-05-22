be46a2df9bb3c65439615711c8fc8d2c7efa0cbc
Ankur <ankur.rangi08@gmail.com>
2024-05-23 00:08:23 +0530

    Signup Endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const zod = require('zod')
+const jwt = require('jsonwebtoken')
 
+const JWT_SECRET = require('../config')
+const { User } = require('../db')
 
 const router = express.Router()
-exports.router = router
 
 const signupSchema = zod.object({
     username: zod.string().email(),
@@ -11,6 +13,45 @@ const signupSchema = zod.object({
     firstName: zod.string(),
     lastName: zod.string(),
 })
-exports.signupSchema = signupSchema
+
+// Signup API
+router.post('/signup', async (req, res) => {
+    const body = req.body
+    const { success } = signupSchema.safeparse(body)
+
+    if (!success) {
+        res.status(411).json({
+            message: 'Incorrect inputs',
+        })
+    }
+
+    const existingUser = await User.findOne({
+        username: req.body.username,
+    })
+
+    if (existingUser) {
+        res.status(411).json({
+            message: 'Email already exists',
+        })
+    }
+
+    //Generally send an email verification.
+
+    const dbUser = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
+
+    const userId = dbUser._id
+
+    const token = jwt.sign({ userId }, JWT_SECRET)
+
+    res.json({
+        message: 'User created successfully',
+        token: token,
+    })
+})
 
 module.exports = router
